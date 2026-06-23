fe3c0e46a993e7e409bb36cb5948d8ed51de4606
davipdev <odavi1909@gmail.com>
2026-06-23 20:32:05 -0300

    frontend add

--- prisma/migrations/20260623224057_usuario/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `usuarioId` to the `historicoConsultas` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "historicoConsultas" ADD COLUMN     "usuarioId" INTEGER NOT NULL;
